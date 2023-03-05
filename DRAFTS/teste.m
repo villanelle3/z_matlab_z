6ce7aa0540d90691378bfe5cc319aefde5ad58de
Laura Augusta <laurah.algusta@gmail.com>
2023-03-05 12:36:04 -0300

    draft file

--- DRAFTS/teste.m
+++ DRAFTS/teste.m
@@ -1,18 +1,32 @@
-global N
-N = 50;
+% global N
+% N = 50;
 
-itens = ["casa", "cinco", "cebola"];
-tam = length(itens);
-for i=1:tam
-    disp(itens(i))
-end
+% itens = ["casa", "cinco", "cebola"];
+% tam = length(itens);
+% for i=1:tam
+%     disp(itens(i))
+% end
 
 % forLoop()
+% 
+% function forLoop()
+%     global N
+%     for i = 0:0.1:N
+%         disp(i)
+%     end
+% end
 
-function forLoop()
-    global N
-    for i = 0:N
-        disp(i)
+% f(x) = 4x^3 - 3*tan^2(x)
+dx = 0.01;
+resp = 1;
+x = 10;
+toggle = true;
+while toggle
+    resp = 4*x^3 - 3*tan(x)*tan(x);
+    if resp < 0.5 && resp > 0
+        toggle = false;
+    else
+        x = x - dx;
     end
 end
 
